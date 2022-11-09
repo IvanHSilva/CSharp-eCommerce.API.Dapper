fa4a7b1a5a67e406f03c6c8c6b4f9f07633dfc95
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-11-09 11:59:08 -0300

    GetUser remaded with Dapper

--- eCommerce.API.Dapper/Repositories/UserRepository.cs
+++ eCommerce.API.Dapper/Repositories/UserRepository.cs
@@ -3,26 +3,22 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using Dapper;
 
 namespace eCommerce.API.Dapper.Repositories {
     public class UserRepository : IUserRepository {
 
         private IDbConnection _connection;
-        public UserRepository() {
 
+        public UserRepository() {
             _connection = new SqlConnection("Data Source=SERVIDOR\\SQLSERVER;Initial Catalog=eCommerce;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
 
-        private static List<User> _dbUsers = new List<User>() {
-            new User(1, "José Rodrigues", "jrodrigues@gmail.com"),
-            new User(2, "Maria Teresa", "marite@gmail.com"),
-            new User(3, "Ronaldo Amaral", "roamaral@gmail.com"),
-            new User(4, "Ana Clarice Mendes", "anacmendes@gmail.com"),
-            new User(5, "Xavier Oliveria", "xaoliver@gmail.com"),
-        };
-
+        //Dapper - MER <-> POO
         public List<User> GetUsers() {
-            return _dbUsers;
+            string command = "SELECT Id, Nome AS Name, Email, Sexo AS Gender, RG, CPF, Filiacao AS Filiation, " +
+                "Situacao AS Situation, DataCad AS RegDate FROM Usuarios";
+            return _connection.Query<User>(command).ToList();
         }
 
         public User GetUser(int id) {
@@ -44,5 +40,12 @@ namespace eCommerce.API.Dapper.Repositories {
             _dbUsers.Remove(_dbUsers.FirstOrDefault(u => u.Id == id));
         }
 
+        private static List<User> _dbUsers = new List<User>() {
+            new User(1, "José Rodrigues", "jrodrigues@gmail.com"),
+            new User(2, "Maria Teresa", "marite@gmail.com"),
+            new User(3, "Ronaldo Amaral", "roamaral@gmail.com"),
+            new User(4, "Ana Clarice Mendes", "anacmendes@gmail.com"),
+            new User(5, "Xavier Oliveria", "xaoliver@gmail.com"),
+        };
     }
 }
