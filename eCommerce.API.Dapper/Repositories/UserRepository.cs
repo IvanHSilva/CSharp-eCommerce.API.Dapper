b14855df79fe40e17e6f8817f5efe70fec7762f8
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-11-09 16:05:30 -0300

    GetUser by Id remaded with Dapper

--- eCommerce.API.Dapper/Repositories/UserRepository.cs
+++ eCommerce.API.Dapper/Repositories/UserRepository.cs
@@ -9,6 +9,7 @@ namespace eCommerce.API.Dapper.Repositories {
     public class UserRepository : IUserRepository {
 
         private IDbConnection _connection;
+        private string _command;
 
         public UserRepository() {
             _connection = new SqlConnection("Data Source=SERVIDOR\\SQLSERVER;Initial Catalog=eCommerce;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
@@ -16,13 +17,15 @@ namespace eCommerce.API.Dapper.Repositories {
 
         //Dapper - MER <-> POO
         public List<User> GetUsers() {
-            string command = "SELECT Id, Nome AS Name, Email, Sexo AS Gender, RG, CPF, Filiacao AS Filiation, " +
-                "Situacao AS Situation, DataCad AS RegDate FROM Usuarios";
-            return _connection.Query<User>(command).ToList();
+            _command = "SELECT Id, Nome AS Name, Email, Sexo AS Gender, RG, CPF, ";
+            _command += "Filiacao AS Filiation, Situacao AS Situation, DataCad AS RegDate FROM Usuarios";
+            return _connection.Query<User>(_command).ToList();
         }
 
         public User GetUser(int id) {
-            return _dbUsers.FirstOrDefault(u => u.Id == id);
+            _command = "SELECT Id, Nome AS Name, Email, Sexo AS Gender, RG, CPF, Filiacao AS Filiation, ";
+            _command += "Situacao AS Situation, DataCad AS RegDate FROM Usuarios WHERE Id = @Id";
+            return _connection.QueryFirstOrDefault<User>(_command, new { Id = id });
         }
 
         public void InsertUser(User user) {
